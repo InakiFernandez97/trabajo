b0d608649f5eb193bb90c3f57973e9d3abd11b3c
jajaraa <134719405+jajaraa@users.noreply.github.com>
2024-05-17 17:06:32 -0400

    se cambió contraseña de test a testing1

--- js/login.js
+++ js/login.js
@@ -1,14 +1,12 @@
 function validarLogin() {
-    // Obtener los valores de los campos de email y password
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
   
-    // Verificar las credenciales (esto es solo un ejemplo, debes reemplazarlo con tu lógica real)
-    if (email === 'test@duoc.cl' && password === 'test1') {
+    if (email === 'test@duoc.cl' && password === 'testing1') {
       alert('Ingreso correcto');
-      return true; // Permitir que el formulario se envíe
+      return true; 
     } else {
       alert('Credenciales incorrectas');
-      return false; // Evitar que el formulario se envíe
+      return false;
     }
   }
