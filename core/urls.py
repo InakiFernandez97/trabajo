024fa7157109f88a782b7aec53f3053ac4cd88ba
jajaraa <134719405+jajaraa@users.noreply.github.com>
2024-06-02 18:28:00 -0400

    Migración realizada y realización primer modelo
    - Se hizo migración a django de toda la página
    - Se hizo el primer modelo en django

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,9 @@ from .views import *
 urlpatterns = [
     path('', index, name='index'),
     path('registro', registro, name='registro'),
+    path('catalogo/', catalogo, name='catalogo'),
+    path('iphone-14-pro/', iphone14pro, name='iphone14pro'),
+    path('samsung-galaxy-s23-ultra/', galaxys23ultra, name='galaxys23ultra'),
+    path('nosotros/', nosotros, name='nosotros'),
+    path('contacto/', contacto, name='contacto'),
 ]
--- core/views.py
+++ core/views.py
@@ -8,3 +8,17 @@ def index(request):
 def registro(request):
     return render(request, 'core/registro.html')
     
+def catalogo(request):
+    return render(request, 'core/catalogo.html')
+
+def nosotros(request):
+    return render(request, 'core/nosotros.html')
+
+def contacto(request):
+    return render(request, 'core/contacto.html')
+
+def iphone14pro(request):
+    return render(request, 'core/iphone-14-pro.html')
+
+def galaxys23ultra(request):
+    return render(request, 'core/samsung-galaxy-s23-ultra.html')
